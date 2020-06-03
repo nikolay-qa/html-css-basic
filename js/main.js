536be579efb75cca25690b5ab690fab0502c5b61
Nikolay Romeiko <nickolay.romeyko@gmail.com>
2020-06-03 22:30:16 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.fairy-tail__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow_prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow_next.svg" alt=""></button>',
+        autoplay: true,
+        fade: true,
+    });
+});
